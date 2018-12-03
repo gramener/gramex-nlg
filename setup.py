18d0559796f4b5b4c75a844a81209d03e4ec4538
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2018-12-03 15:11:35 +0530

    Basic repo setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+"""
+NLG Setup.
+"""
+
+from setuptools import setup, find_packages
+setup(name='kepler',
+      version='0.0.1',
+      py_modules=find_packages(),
+      )
