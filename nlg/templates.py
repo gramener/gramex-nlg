18ce01cd9161411aec0f13b5bedd7d9e6d85c173
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2018-12-23 09:38:27 +0530

    Refactoring and simplification of the DSL
    According to points in #10

--- nlg/templates.py
+++ nlg/templates.py
@@ -7,6 +7,7 @@ Templates used in Gramex NLG.
 """
 import json
 import random
+import re
 from string import Formatter
 
 import pandas as pd
@@ -18,100 +19,183 @@ TEMPLATES = {
     'comparison': '{subject} {verb} {quant} {adjective} than {object}.'
 }
 
+FUNC_EXPR = re.compile(r'^(?P<filter>[a-z|A-Z]{1}[a-z|A-Z|_|\d]+)\((?P<colname>.*)\)')
+
+
+def parse_func_expr(expr):
+    m = re.match(FUNC_EXPR, expr)
+    if m is None:
+        raise ValueError('Invalid Expression')
+    return {'colname': m.group('colname'), 'filter': m.group('filter')}
+
 
 class Narrative(object):
 
     def __init__(self, struct):
-        self.intent = struct['intent']
-        self.template = TEMPLATES[self.intent]
+        intent = struct.get('intent', False)
+        if intent:
+            self.intent = intent
+            self.template = TEMPLATES[self.intent]
+        else:
+            if 'template' not in struct:
+                raise KeyError('Either intent or template must be specified.')
+            self.template = struct['template']
         self.metadata = struct['metadata']
         self.data = struct['data']
 
-    @property
-    def subject(self):
-        subject = self.metadata['subject']
-        if isinstance(subject, str):
-            return subject
-        if isinstance(subject, dict):
-            tmpl = subject.get('template', False)
-            if tmpl:
-                fmt_kwargs = {}
-                for _, fname, _, _, in Formatter().parse(tmpl):
-                    if fname:
-                        fmt_kwargs[fname] = self.kwarg_from_df(
-                            **subject['kwargs'][fname])
-                return tmpl.format(**fmt_kwargs)
-            return self.kwarg_from_df(**subject)
-        raise TypeError('Subject not found.')
+    def is_template(self, spec):
+        """Check if a spec contains a template and the information required to
+        render that template.
 
-    @property
-    def quant(self):
-        quant = self.metadata['quant']
-        if isinstance(quant, str):
-            return quant
-        if isinstance(quant, dict):
-            tmpl = quant.get('template', False)
-            if tmpl:
-                fmt_kwargs = {}
-                for _, fname, _, _ in Formatter().parse(tmpl):
-                    if fname:
-                        fmt_kwargs[fname] = self.eval_quant(
-                            **quant['kwargs'][fname])
-            return tmpl.format(**fmt_kwargs)
-        raise TypeError('Quant not found.')
+        Parameters
+        ----------
+        spec : dict
 
-    @property
-    def verb(self):
-        verb = self.metadata['verb']
-        if isinstance(verb, str):
-            return verb
-        if isinstance(verb, (list, tuple)):
-            return random.choice(verb)
+        Returns
+        -------
+        bool
+            Whether the given spec contains a template.
+        """
+        template = spec.get('template')
+        if isinstance(template, str):
+            fieldnames = [f for _, f, _, _ in Formatter().parse(template) if f]
+            kwargs = spec.get('kwargs', {})
+            if kwargs:
+                return all([f in kwargs for f in fieldnames])
+            return all([f in template for f in fieldnames])
+        return False
 
-    @property
-    def adjective(self):
-        adj = self.metadata['adjective']
-        if isinstance(adj, str):
-            return adj
-        if isinstance(adj, (list, tuple)):
-            return random.choice(adj)
+    def get_template_kwargs(self, spec):
+        """Parse a template dict and find the fieldname kwargs."""
+        tmpl = spec.get('template')
+        fieldnames = [f for _, f, _, _ in Formatter().parse(tmpl) if f]
+        kwargs = spec.get('kwargs', False)
+        if kwargs:
+            for f in fieldnames:
+                if f not in kwargs:
+                    raise KeyError("Fieldname '{}' not found in kwargs.".format(f))
+            return {f: kwargs[f] for f in fieldnames}
+        else:
+            for f in fieldnames:
+                if f not in spec:
+                    raise KeyError("Fieldname '{}' not found in spec.".format(f))
+            return {f: spec[f] for f in fieldnames}
 
-    @property
-    def object(self):
-        obj = self.metadata['object']
-        if isinstance(obj, str):
-            return obj
-        if isinstance(obj, dict):
-            tmpl = obj['template']
-            fmt_kwargs = {}
-            for _, fname, _, _ in Formatter().parse(tmpl):
-                if fname:
-                    fmt_kwargs[fname] = self.kwarg_from_df(
-                        **obj['kwargs'][fname])
-            return tmpl.format(**fmt_kwargs)
+    def process_template(self, spec):
+        """Process a template."""
+        tmpl = spec['template']
+        kwargs = self.get_template_kwargs(spec)
+        fmt_kwargs = {}
+        for k, v in kwargs.items():
+            if self.is_filter(v):
+                fmt_kwargs[k] = self.process_filter(**v)
+            elif self.is_quant_operation(v):
+                fmt_kwargs[k] = self.eval_quant(**v)
+        return tmpl.format(**fmt_kwargs)
+
+    def is_filter(self, spec):
+        """Check if a spec represents a filter."""
+        return all([c in spec for c in ('_type', 'colname', '_filter')])
+
+    def process_filter_dict(self, colname, _filter):
+        by = _filter['colname']
+        subfilter = _filter['filter']
+        ix = getattr(self.data[by], 'idx' + subfilter)()
+        return self.data.iloc[ix][colname]
+
+    def process_filter(self, _type, colname, _filter):
+        value = None
+        if isinstance(_filter, str):
+            try:
+                _filter = parse_func_expr(_filter)
+                value = self.process_filter_dict(colname, _filter)
+            except ValueError:
+                value = get_series_extreme(self.data[colname], _filter)
+        elif isinstance(_filter, dict):
+            value = self.process_filter_dict(colname, _filter)
+        return value
+
+    def is_quant_operation(self, spec):
+        return ('_type' in spec) and ('expr' in spec)
 
     def eval_quant(self, _type, expr):
         if _type != 'operation':
             return 0
         return pd.eval(expr.format(data=self.data))
 
-    def kwarg_from_df(self, _type, colname, _filter):
-        value = None
-        if isinstance(_filter, str):
-            value = get_series_extreme(self.data[colname], _filter)
-        elif isinstance(_filter, dict):
-            by = _filter['colname']
-            subfilter = _filter['filter']
-            ix = getattr(self.data[by], 'idx' + subfilter)()
-            value = self.data.iloc[ix][colname]
-        return value
+    def process_pos(self, spec):
+        """Process the specification for an arbitrary part of speech.
+
+        Parameters
+        ----------
+        spec : string or dict
+            The specification for a given part of speech. If string, it is
+            assumed to be a literal. If dict, it is expected to contain filters
+            which allow it's calculation.
+
+
+        Returns
+        -------
+        str
+            Rendered string representation of the PoS.
+        """
+        if isinstance(spec, str):  # literal string
+            return spec
+        if isinstance(spec, (list, tuple)):
+            return random.choice(spec)
+        if self.is_template(spec):  # template
+            return self.process_template(spec)
+        if self.is_filter(spec):  # literal, but inferred from a filter
+            return self.process_filter(**spec)
+
+    @property
+    def subject(self):
+        subject = self.metadata.get('subject', False)
+        if subject:
+            return self.process_pos(subject)
+        raise KeyError('Subject not found.')
+
+    @property
+    def quant(self):
+        quant = self.metadata.get('quant', False)
+        if quant:
+            return self.process_pos(quant)
+        raise KeyError('quant not found.')
+
+    @property
+    def verb(self):
+        verb = self.metadata.get('verb', False)
+        if verb:
+            return self.process_pos(verb)
+        raise KeyError('verb not found.')
+
+    @property
+    def adjective(self):
+        adjective = self.metadata.get('adjective', False)
+        if adjective:
+            return self.process_pos(adjective)
+        raise KeyError('adjective not found.')
+
+    @property
+    def object(self):
+        obj = self.metadata.get('object', False)
+        if obj:
+            return self.process_pos(obj)
+        raise KeyError('Object not found.')
 
     def render(self):
         fmt_kwargs = {}
         for _, fieldname, _, _ in Formatter().parse(self.template):
             if fieldname:
-                fmt_kwargs[fieldname] = getattr(self, fieldname,
-                                                '{{}}'.format(fieldname))
+                field_args = getattr(self, fieldname, False)
+                if not field_args:
+                    spec = self.metadata.get(fieldname, False)
+                    if not spec:
+                        raise KeyError("Cannot find {}.".format(fieldname))
+                    fmt_kwargs[fieldname] = self.process_pos(spec)
+                else:
+                    fmt_kwargs[fieldname] = getattr(self, fieldname,
+                                                    '{{}}'.format(fieldname))
         return self.template.format(**fmt_kwargs)
 
 
