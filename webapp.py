fa29785801d3abf3f44d21a41108fa0defa20ccb
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2019-02-11 19:30:39 +0530

    Allow selecting from uploaded files

--- webapp.py
+++ webapp.py
@@ -7,9 +7,13 @@ Module for gramex exposure. This shouldn't be imported anywhere, only for use
 with gramex.
 """
 import json
+import os
 import os.path as op
+import shutil
 from urllib import parse
 
+from gramex.config import variables
+from gramex import conf
 import pandas as pd
 from tornado.template import Template
 
@@ -18,16 +22,47 @@ from nlg import grammar as G
 from nlg import templatize
 from nlg import utils as U
 
-fpath = op.join(op.dirname(__file__), 'app/gramupload/file.csv')
+fh_fpath = op.join(op.dirname(__file__), 'app/gramupload/file.csv')
+grx_data_dir = variables['GRAMEXDATA']
+nlg_path = op.join(grx_data_dir, 'nlg')
 
-if op.isfile(fpath):
-    orgdf = pd.read_csv(fpath)
+if not op.isdir(nlg_path):
+    os.mkdir(nlg_path)
+
+if op.isfile(fh_fpath):
+    orgdf = pd.read_csv(fh_fpath)
 
 
 def _watchfile_loader(event):
     global orgdf
     if op.isfile(event.src_path):
         orgdf = pd.read_csv(event.src_path)
+
+
+def copy_file_to_user_dir(meta, handler):
+    for k, v in conf['url'].items():
+        if k.startswith('nlg-uploadhandler'):
+            break
+    src_dir = v['kwargs']['path']
+    dest_dir = op.join(nlg_path, handler.current_user.email)
+    if not op.isdir(dest_dir):
+        os.mkdir(dest_dir)
+    dest_path = op.join(dest_dir, meta.filename)
+    shutil.copy(op.join(src_dir, 'file.csv'), dest_path)
+
+
+def get_dataset_files(handler):
+    user_dir = op.join(nlg_path, handler.current_user.email)
+    if op.isdir(user_dir):
+        return os.listdir(user_dir)
+    return []
+
+
+def make_dataset_list(content, handler):
+    t = Template(content)
+    return t.generate(
+        handler=handler,
+        NLG_DATASETS=get_dataset_files(handler)).decode('utf-8')
 
 
 def render_template(handler):
@@ -89,3 +124,9 @@ def get_gramopts(handler):
         if getattr(obj, 'gramopt', False):
             funcs[obj.fe_name] = {'source': obj.source, 'func_name': attrname}
     return funcs
+
+
+def use_existing_file(handler):
+    fname = handler.args['dataset'][0]
+    user_dir = op.join(nlg_path, handler.current_user.email)
+    shutil.copy(op.join(user_dir, fname), fh_fpath)
