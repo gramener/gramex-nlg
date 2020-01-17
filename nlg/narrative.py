a207674394ef19e1beae3be0b736999dba379e21
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2020-01-17 17:02:06 +0530

    ENH: Add variables and formulae to nuggets

--- nlg/narrative.py
+++ nlg/narrative.py
@@ -5,13 +5,23 @@
 """The Narrative class."""
 import json
 import re
+import warnings
 
+from spacy.tokens import Token
 from tornado.template import Template
 
 from nlg import utils, grammar
 
 t_templatize = lambda x: '{{ ' + x + ' }}'  # noqa: E731
 nlp = utils.load_spacy_model()
+
+
+def _check_unique_token(t, doc):
+    if len([c for c in doc if c.text == t]) > 1:
+        msg = f'There is more than one token in the document that matches the text "{t}".' \
+            + " Using the first match." \
+            + " Please use a `spacy.token.Token` instance for searching."
+        warnings.warn(msg)
 
 
 class Variable(object):
@@ -26,6 +36,10 @@ class Variable(object):
         if inflections is None:
             inflections = []
         self.inflections = inflections
+
+    def set_expr(self, expr):
+        tmpl = self.enabled_source
+        tmpl['tmpl'] = expr
 
     @property
     def enabled_source(self):
@@ -56,6 +70,9 @@ class Variable(object):
             tmplstr = f'{source}.{func}({tmplstr})'
         return tmplstr
 
+    def __repr__(self):
+        return self.template
+
 
 class Nugget(object):
     def __init__(self, text, tokenmap=None, inflections=None, fh_args=None,
@@ -82,12 +99,19 @@ class Nugget(object):
     def variables(self):
         return self.tokenmap
 
-    def get_variable(self, t):
-        for token in self.doc:
-            if token.text == t:
-                variable = self.tokenmap.get(token, False)
-                if variable:
-                    return variable
+    def get_var(self, t):
+        if isinstance(t, Token):
+            variable = self.tokenmap.get(t, False)
+            if variable:
+                return variable
+        elif isinstance(t, str):
+            _check_unique_token(t, self.doc)
+            for token in self.doc:
+                if token.text == t:
+                    variable = self.tokenmap.get(token, False)
+                    if variable:
+                        return variable
+        raise KeyError('Variable not found.')
 
     @property
     def template(self):
@@ -106,22 +130,26 @@ class Nugget(object):
     def __repr__(self):
         return self.template
 
-    def render(self, df, fh_args=None):
+    def render(self, df, fh_args=None, **kwargs):
         if fh_args is not None:
             self.fh_args = fh_args
-        return Template(self.template, whitespace='oneline').generate(orgdf=df, U=utils, G=grammar)
+        else:
+            fh_args = {}
+        kwargs['fh_args'] = fh_args
+        return Template(
+            self.template, whitespace='oneline').generate(orgdf=df, U=utils, G=grammar, **kwargs)
 
     def add_fh_args(self, sent):
         if self.fh_args:
             fh_args = json.dumps(self.fh_args)
             tmpl = f'{{% set fh_args = {fh_args}  %}}\n'
             tmpl += f'{{% set df = U.gfilter(orgdf, fh_args.copy()) %}}\n'
-            tmpl += f'{{% fh_args = U.sanitize_fh_args(fh_args) %}}\n'
+            tmpl += f'{{% set fh_args = U.sanitize_fh_args(fh_args, orgdf) %}}\n'
             tmpl += '{# Do not edit above this line. #}\n'
             return tmpl + sent
         return sent
 
-    def set_variable(self, token, varname='', expr=''):
+    def add_var(self, token, varname='', expr=''):
         if not (varname or expr):
             raise ValueError('One of `varname` or `expr` must be provided.')
         if isinstance(token, int):
