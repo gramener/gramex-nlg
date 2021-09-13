adeb291c0ca97f38e8114b20eb24ce8cb97d6730
Venkata Krishna Reddy <venkata.reddy@gramener.org>
2021-09-13 13:01:28 +0530

    commit

--- Roberta.py
+++ Roberta.py
@@ -1,7 +1,7 @@
 import os
 from random import randint
 
-for i in range(1,4):
+for i in range(0,1):
     for j in range(0, randint(1,10)):
         d = str(i)+'days ago'
         with open('file.txt', 'a') as file:
